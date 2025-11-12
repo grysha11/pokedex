9d602289f74a77ac9acee046215def16c624a400
grysha11 <grzhzahar@gmail.com>
2025-11-12 19:56:23 +0100

    add getLocation commands

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -3,12 +3,13 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"github.com/grysha11/pokedex/api"
 )
 
 type CliCommand struct {
 	Name		string
 	Description	string
-	Callback	func() error
+	Callback	func(cfg *api.Config) error
 }
 
 var Cmds map[string]CliCommand
@@ -25,21 +26,57 @@ func init() {
 			Description:	"Displays a help message",
 			Callback:		CommandHelp,
 		},
+		"map": {
+			Name:			"map",
+			Description:	"Displays the names of next 20 location areas",
+			Callback:		CommandMap,
+		},
+		"mapb": {
+			Name:			"mapb",
+			Description:	"Displays the names of previous 20 location areas",
+			Callback:		CommandMapB,
+		},
 	}
 }
 
-func CommandExit() error {
+func CommandExit(cfg *api.Config) error {
 	fmt.Println("Closing the Pokedex... Goodbye!")
 	os.Exit(0)
 	return nil
 }
 
-func CommandHelp() error {
+func CommandHelp(cfg *api.Config) error {
 	fmt.Println("\nWelcome to the Pokedex!")
 	fmt.Println("Usage:")
 
 	for _, cmd := range Cmds {
 		fmt.Printf(" %s: %s\n", cmd.Name, cmd.Description)
 	}
+	return nil
+}
+
+func CommandMap(cfg *api.Config) error {
+	locationData, err := api.GetLocationAreas(true, cfg)
+	if err != nil {
+		return err
+	}
+
+	for _, location := range locationData.Results {
+		fmt.Printf("%v\n", location.Name)
+	}
+
+	return nil
+}
+
+func CommandMapB(cfg *api.Config) error {
+	locationData, err := api.GetLocationAreas(false, cfg)
+	if err != nil {
+		return err
+	}
+
+	for _, location := range locationData.Results {
+		fmt.Printf("%v\n", location.Name)
+	}
+
 	return nil
 }
--- repl/repl.go
+++ repl/repl.go
@@ -6,9 +6,20 @@ import (
 	"os"
 	"github.com/grysha11/pokedex/util"
 	"github.com/grysha11/pokedex/cmd"
+	"github.com/grysha11/pokedex/api"
 )
 
+func initConfig() (*api.Config) {
+	initialURL := "https://pokeapi.co/api/v2/location-area/"
+	cfg := &api.Config{
+		NextLocationArea: &initialURL,
+		PrevLocationArea: nil,
+	}
+	return cfg
+}
+
 func Start() {
+	cfg := initConfig()
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
 		fmt.Printf("Pokedex > ")
@@ -26,7 +37,7 @@ func Start() {
 			continue
 		}
 
-		err := command.Callback()
+		err := command.Callback(cfg)
 		if err != nil {
 			fmt.Printf("Error occurred: %v\n", err)
 		}
